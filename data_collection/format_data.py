40bf8c8c166241602a12e7ae3cbc9cb966870806
atulesamuel20@gmail.com <atulesamuel20@gmail.com>
2020-03-29 23:07:38 +0000

    edited format_data.py to loop through all sub_folders

--- data_collection/format_data.py
+++ data_collection/format_data.py
@@ -4,7 +4,7 @@ import csv
 import re
 
 
-def format_data(videos_path):
+def format_data(sub_folders):
     """
     Moves videos to target folder for model to train on.
 
@@ -12,21 +12,30 @@ def format_data(videos_path):
     ---------
         video_path: path to collected video with json target text included.
     """
-    videos = sorted(glob.glob(videos_path + "/*.mp4"))
-    csv_text = sorted(glob.glob(videos_path + "/*.csv"))
+    # loop throught all sub-folders
+    count = 0
+    for videos_path in sub_folders:
 
-    # read in csv data
-    data_dict = {}
-    with open(csv_text[0], newline='') as f:
-        reader = list(csv.reader(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL))
-        for ln in reader:
-            data_dict[ln[0]] = re.sub('[:,.)(?!;*"-]', "", ln[1].lower()) 
+        videos = sorted(glob.glob(videos_path + "/*.mp4"))
+        csv_text = sorted(glob.glob(videos_path + "/*.csv"))
 
-    for vid in videos:
-        key = vid.split("\\")[-1].split(".")[0]
-        print(vid, data_dict[key])
+        # read in csv data
+        data_dict = {}
+        with open(csv_text[0], newline='') as f:
+            reader = list(csv.reader(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL))
+            for ln in reader:
+                data_dict[ln[0]] = re.sub('[:,.)(?!;*"-]', "", ln[1].lower()) 
 
+        for vid in videos:
+            key = vid.split("\\")[-1].split(".")[0]
+            print(vid, data_dict[key])
+            count += 1
+            
+    print("total data points: ", count)
 
 if __name__ == "__main__":
-    videos_path = "processed_data/goals/"
-    format_data(videos_path)
+    sub_folders = ["processed_data/downloaded/", "processed_data/goals/",
+                    "processed_data/holiday/", "processed_data/school/",
+                    "processed_data/summer/", "processed_data/vocab/"]
+
+    format_data(sub_folders)
