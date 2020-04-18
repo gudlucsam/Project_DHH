4838930162b5b41bd5ef0efa876082a63f0b9737
atulesamuel20@gmail.com <atulesamuel20@gmail.com>
2020-04-18 18:28:56 +0000

    adding model selection

--- web_app/app.py
+++ web_app/app.py
@@ -1,6 +1,10 @@
 import os
 import time
+import sys
+
 import numpy as np
+
+sys.path.append('C:\\Users\\atule\\Desktop\\Project_DNN')
 
 from model_lib import config
 from model_lib.utility_functions import target_text_encoder, process_videos, images_normalize
@@ -9,76 +13,73 @@ from model_lib.videocapture import predict_from_camera
 from model_lib.camera import VideoCamera
 
 from flask_cors import CORS
-from flask import Flask, jsonify, make_response, Response, request
+from flask import Flask, jsonify, make_response, Response, request, render_template, redirect, url_for
+
 
 
 
 # instantiate flask
 app = Flask(__name__)
-cors = CORS(app, resources={r"/ml/api/v1.0/*": {"origins": 'http://localhost:4200'}})
 # keras model
 model = None
 # frames list
 liFrames = []
 status = True
 
-@app.route("/ml/api/v1.0/info")
+@app.route('/')
 def index():
-    return jsonify({
-        "project_name":"Undergraduate Capstone Project",
-        "model_name":"dhh-asl-api-v1.0",
-        "version":"v1.0",
-        "author":"Samuel Atule",
-        "email":"atulesamuel20@gmail.com"
-    })
+    select_form_vals = [(0, "Mobilenet(Recommended for slow device)"), (1, "InceptionV3")]
+    default = 0
+    if request.args.get("default"):
+        default = request.args.get("default")
+    return render_template('index.html', form_vals=select_form_vals, default = default)
 
 
-@app.route("/ml/api/v1.0/md/<int:model_id>", methods=['GET'])
-def select_feature_extraction_model(model_id):
-
-    # init response
-    res = {"success": False}
+@app.route("/model", methods=['POST'])
+def select_feature_extraction_model():
 
     # make model instance global
     global model
 
-    # select inception if model_id is 0 else mobilenet
-    if model_id == 0:
-        config.params["cnn_model_params"] = config.inception_model_params
-        config.params["nFeatureLength"] = config.inception_model_params["output_shape"]
-        config.params["saved_model_path"] = config.inception_model_params["saved_model_path"]
-    elif model_id == 1:
-        config.params["cnn_model_params"] = config.mobilenet_model_params
-        config.params["nFeatureLength"] = config.mobilenet_model_params["output_shape"]
-        config.params["saved_model_path"] = config.mobilenet_model_params["saved_model_path"]
-    else:
-        return jsonify(res)
+    if request.method == 'POST':
+        default = 0
+        model_id = int(request.form.get("selectmodel"))
 
-    # build encoder - decoder model
-    model = lstm_models(**config.params)
+        # select inception if model_id is 0 else mobilenet
+        if model_id == 0:
+            config.params["cnn_model_params"] = config.mobilenet_model_params
+            config.params["nFeatureLength"] = config.mobilenet_model_params["output_shape"]
+            config.params["saved_model_path"] = config.mobilenet_model_params["saved_model_path"]
+            #  select chosen option
+            default = 0
 
-    # retrieve param
-    videos_path = config.videos_path
-    nResizeMinDim = config.nResizeMinDim
+        elif model_id == 1:
+            config.params["cnn_model_params"] = config.inception_model_params
+            config.params["nFeatureLength"] = config.inception_model_params["output_shape"]
+            config.params["saved_model_path"] = config.inception_model_params["saved_model_path"]
+            #  select chosen option
+            default = 1
 
-    # train or load model
-    model.train(videos_path, nResizeMinDim)
+        else:
+            default = 0
+            return redirect( url_for('index', default=default))
 
-    # return success
-    res["success"] = True
+        # build encoder - decoder model
+        model = lstm_models(**config.params)
 
-    return jsonify(res)
+        # retrieve param
+        videos_path = config.videos_path
+        nResizeMinDim = config.nResizeMinDim
+
+        # train or load model
+        model.train(videos_path, nResizeMinDim)
+
+        return redirect( url_for('index', default=default))
 
 
 def predict():
     global model
     global liFrames
-
-    # init response
-    res = {
-        "success": False,
-        "prediction": []
-        }
 
     # retrieve params from config
     nTargetFrames = config.params["nTargetFrames"]
@@ -90,28 +91,19 @@ def predict():
 
     # predict from live feeds
     prediction = model.predict([video_frames])
-    
-    # check if prediction successful otherwise return unsuccessful
-    if not prediction:
-        return jsonify(res)
 
-    # return success
-    res["success"] = True
-    res["prediction"] = prediction
-
-    return jsonify(res)
+    return prediction
 
 
 def gen(camera, nTimeDuration = 4):
     global liFrames
-    global status
-    fTimeStart = time.time()
+    # fTimeStart = time.time()
 
     liFrames = []
-    while status != False:
+    while True:
         # stop after nTimeDuration sec
-        fTimeElapsed = time.time() - fTimeStart
-        if fTimeElapsed > nTimeDuration: break
+        # fTimeElapsed = time.time() - fTimeStart
+        # if fTimeElapsed > nTimeDuration: break
         # capsture frames
         frame, img = camera.get_frame()
         # append images for prediction
@@ -121,23 +113,9 @@ def gen(camera, nTimeDuration = 4):
         yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
-@app.route('/ml/api/v1.0/md/vf', methods=["POST"])
+@app.route('/ml/api/v1.0/md/vf')
 def video_feed():
-    global status
-
-    if request.method == "POST":
-        payload = request.get_json("status").get("status", None)
-        if payload == True:
-            status = True
-            return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
-        elif payload == False:
-            status = False
-            # Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
-            return predict()
-        else:
-            return jsonify({
-                "error": " Cannot reach camera",
-            })
+    return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @app.errorhandler(404)
